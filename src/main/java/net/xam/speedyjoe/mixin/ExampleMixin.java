406ca1662ffcd585a0d3943c544bddff93448d15
xam290 <schmax9999@gmail.com>
2023-03-10 22:43:55 +0100

    Initial commit

---
+++ src/main/java/net/xam/speedyjoe/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package net.xam.speedyjoe.mixin;
+
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.xam.speedyjoe.SpeedyJoe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		SpeedyJoe.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
